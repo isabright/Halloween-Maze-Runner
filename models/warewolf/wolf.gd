9c1259bc5a8b70f2f91d2238a27e60f494a70685
Isabelle Bright <131718387+isabright@users.noreply.github.com>
2025-12-17 17:21:52 -0600

    move spawners forward slightly

--- models/warewolf/wolf.gd
+++ models/warewolf/wolf.gd
@@ -1,6 +1,8 @@
 extends Node3D
-
+@onready var candy = load("res://candycord.tscn")
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	visible = false
+	var candy_instance = candy.instantiate()
+	add_child(candy_instance)
 	
--- models/warewolf/wolf3.gd
+++ models/warewolf/wolf3.gd
@@ -1,5 +1,8 @@
 extends Node3D
-
+@onready var candy = load("res://candycord.tscn")
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	pass # Replace with function body.
+	visible = false
+	var candy_instance = candy.instantiate()
+	add_child(candy_instance)
+	
