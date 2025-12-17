508fbc59111c57dcca21c1a9b9464131c27ff687
Isabelle Bright <131718387+isabright@users.noreply.github.com>
2025-12-17 17:05:36 -0600

    wolf scripts

---
+++ models/warewolf/wolf.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	visible = false
+	
---
+++ models/warewolf/wolf4.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	pass # Replace with function body.
